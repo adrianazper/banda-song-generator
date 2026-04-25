136b2022ba90c3df6a4bded608d2f114b663122d
adrianazper <adrianazper11@gmail.com>
2026-04-25 13:13:14 -0700

    Fixed spelling in one song

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const songs = [
     "Toro De Once", "Arriba Pichataro", "La Rabia", "La Cuichi", "La Morena", "Que Te Pasa", "Canchame", "La Novia Del Pajarillo",
     "Por Una Mujer Casada", "Cielito Lindo", "El Rey", "Tragos De Amargo Licor", "La Mejor De Todas", "Amor Maldito", "Corazon Duro",
     "Regalo De Dios", "Tiburon", "La Yaquesita", "Pavido Navido", "La Iguana", "La Boda Del Huitlacoche", "Por Ese Calle Vive",
-    "Andamos Mejor", "Puno De Tierra", "Por Ella", "Arrboles De La Barranca", "Un Rinconcito en el Cielo", "Cuatro Meses",
+    "Andamos Mejor", "Puno De Tierra", "Por Ella", "Arboles De La Barranca", "Un Rinconcito en el Cielo", "Cuatro Meses",
     "El Muchacho Alegre", "El Chubasco", "El Molino", "El Centenario", "Reproches Al Viento", "Corrido De Nayarit", "Mi Lindo Nayarit",
     
 ];
