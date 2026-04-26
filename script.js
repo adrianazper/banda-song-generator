e68887f739647f2c28201d1b04b8a9a04f2d10c0
adrianazper <adrianazper11@gmail.com>
2026-04-26 09:59:41 -0700

    added filter option to separate by category for songs

--- script.js
+++ script.js
@@ -1,15 +1,59 @@
-const songs = [
-    "Mi Gusto Es", "Mi Ranchito", "El Tarasco", "Cuanto Me Gusto Este Rancho", "Las Feas", "La Vaquilla",
-    "Carta Esther", "Amor A La Ligera", "La Nena", "Provocame", "Pa Ya Y Paca", "El Esquimal", "Dos Amigos", "30 Cartas", "Ilusion 89",
-    "Toro De Once", "Arriba Pichataro", "La Rabia", "La Cuichi", "La Morena", "Que Te Pasa", "Canchame", "La Novia Del Pajarillo",
-    "Por Una Mujer Casada", "Cielito Lindo", "El Rey", "Tragos De Amargo Licor", "La Mejor De Todas", "Amor Maldito", "Corazon Duro",
-    "Regalo De Dios", "Tiburon", "La Yaquesita", "Pavido Navido", "La Iguana", "La Boda Del Huitlacoche", "Por Ese Calle Vive",
-    "Andamos Mejor", "Puno De Tierra", "Por Ella", "Arboles De La Barranca", "Un Rinconcito en el Cielo", "Cuatro Meses",
-    "El Muchacho Alegre", "El Chubasco", "El Molino", "El Centenario", "Reproches Al Viento", "Corrido De Nayarit", "Mi Lindo Nayarit",
+
+const cumbias = ["Popurri Las Feas (DO y Sib)", "Cauhates Pistaches (RE)", "Provocame (RE)", "Tamarindo (FA)", "La Nena ()",
+  "Popurri Pa Ya Y Paca ()", "Popurri La Yaquesita ()", "Canchame ()", "La Morena ()", "Que Te Pasa ()", "Cinco De Te ()",
+  "La Chica Fresa ()", "Palinos Chinos ()", "Por Ella ()", "Hola Corazon ()", "Si Tu Boquita Fuera ()", "Al Ritmo de la Banda ()",
+  "Bailando Y Quebrando ()",
     
 ];
 
+const rancheras = ["Mi Ranchito (FA)", "Cuanto Me Gusta Este Ranche (FA)", "Por Ese Calle Vive (FA)", "El Tarasco (Sib)",
+  "Carta Esther (FA)", "Amor A La Ligera ()", "Suspiros (FA)", "Cielo (DO)", "Popurri 30 Cartas (Sol y FA)", "Sin Fortuna ()",
+  "Por Una Mujer Casada ()", "Popurri El Rey ()", "Acábame de Matar ()", "Cuatro Meses ()", "El Chubasco (FA)", "Reproches Al Viento ()",
+  "Mi Lindo Nayarit ()", "Caminos De Michoacan ()", "Arboles de la Barranca ()", "Un Puño de Tierra ()", "Por Las Calles De Chihuahua ()",
+  "La Novia Del Pajarillo ()", 
+
+]
+
+const corridos = [
+  "El Molino (DO)", "Dos Amigos ()", "Corrido de Nayarit ()", "El Centenario ()", "Andamos Mejor ()", "Corrido de Juan Martha ()",
+  "El Muchacho Alegre ()"
+]
+
+const zapateados = ["La Vaquilla ()", "El Sinaloense ()", "Ilusion 89 ()", "Toro de Once ()", "Toro Viejo ()", "Arriba Pichataro ()",
+  "La Varquita ()", "La Rabia ()", "La Cuichi ()", "Pavido Navido ()", "Popurri La Boda Del Huitlacoche ()",
+]
+
+const romanticas = ["Amor Maldito ()", "Corazon Duro ()", "Un Rinconcito en el Cielo ()", "En Los Puritos Huesos ()", "La Mejor De Todas ()",
+  "Regalo de Dios ()",
+
+]
+
+const categories = {
+  Cumbias: cumbias,
+  Rancheras: rancheras,
+  Corridos: corridos,
+  Zapateados: zapateados,
+  Romanticas: romanticas
+};
+
 document.getElementById("generateBtn").addEventListener("click", function () {
+  const selected = document.getElementById("myFilter").value;
+
+  let songs;
+
+  if (selected === "All") {
+    songs = [
+      ...cumbias,
+      ...rancheras,
+      ...corridos,
+      ...zapateados,
+      ...romanticas
+    ];
+  } else {
+    songs = categories[selected];
+  }
+
   const random = Math.floor(Math.random() * songs.length);
+
   document.getElementById("result").textContent = songs[random];
 });
